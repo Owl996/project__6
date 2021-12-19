a0bb17e7ace85873214b36a9cb2ffa8a9b0f9ffd
Denis <dlironov@bk.ru>
2021-12-20 02:04:55 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,19 @@
+﻿// ConsoleApplication4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string> 
+using namespace std;
+
+int main()
+{
+    string s1 = { "lc tr ab" };
+    string s2 = { "it ab" };
+
+    for (size_t i = 0; i < s1.size(); i++)
+        for (size_t j = 0; j < s2.size(); j++)
+            if (s1[i] == s2[j]) { cout << i << " "; break; }
+
+    system("pause");
+    return 0;
+}
